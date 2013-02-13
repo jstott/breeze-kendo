59ef1e0dbf720b44e1a71cfa6b7b237a4a52d87a
Derick Bailey <derickbailey@gmail.com>
2013-02-13 12:53:33 -0600

    starting to look at what it takes to do a breeze-datasource adapater for kendo.data.DataSource

---
+++ api/todos.php
@@ -0,0 +1,17 @@
+<?php
+  header("content-type", "application/json");
+?>
+[
+  {
+    "id": 1,
+    "description": "foo"
+  },
+  {
+    "id": 2,
+    "description": "bar"
+  },
+  {
+    "id": 3,
+    "description": "baz"
+  }
+]
---
+++ javascripts/demo.js
@@ -0,0 +1,55 @@
+$(function(){
+
+  // Breeze assumes asp.net mvc4 w/ WebAPI as the default
+  // data services. To not use that, we have to create a
+  // DataService instance and tell it where our end-point is,
+  // and also tell it not to use any metadata
+  var dataService = new breeze.DataService({
+      serviceName: '/api/todos.php',
+      hasServerMetadata: false
+  });
+
+  var manager = new breeze.EntityManager({
+    dataService: dataService
+  });
+
+  // ---------------------------------------------
+  
+  var transport = {
+
+    read: function(options){
+      console.log("reading", options);
+
+      var query = new breeze.EntityQuery("todos")
+        .skip(options.data.skip)
+        .take(options.data.take);
+
+      manager.executeQuery(query).then(function(xhr){
+        options.success(xhr.results);
+      });
+    }
+
+  };
+
+  var ds = new kendo.data.DataSource({
+    transport: transport,
+    autoSync: true,
+    pageSize: 1,
+    serverPaging: true,
+    serverSorting: true,
+    serverFiltering: true
+  });
+
+  $("#grid").kendoGrid({
+    columns: [ 
+      { field: "id" },
+      { field: "description" }
+    ],
+    dataSource: ds,
+    pageable: true,
+    sortable: true,
+    filterable: true
+  });
+
+});
+
